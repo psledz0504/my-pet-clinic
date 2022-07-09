52dcdf74f701439555551cf3bf27eb588a421407
psledz <psledz@cisco.com>
2022-07-09 19:44:10 +0200

    added service interfaces

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/service/VetService.java
@@ -0,0 +1,13 @@
+package com.springframework.petclinic.service;
+
+import com.springframework.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
