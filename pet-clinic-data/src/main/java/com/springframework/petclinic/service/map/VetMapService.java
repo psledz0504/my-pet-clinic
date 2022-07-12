681b355a2381962730850a54a7c6df0ad8ed3ca0
psledz <psledz@cisco.com>
2022-07-12 20:07:17 +0200

    modified VetMapService

--- pet-clinic-data/src/main/java/com/springframework/petclinic/service/map/VetMapService.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/service/map/VetMapService.java
@@ -1,9 +1,30 @@
 package com.springframework.petclinic.service.map;
 
+import com.springframework.petclinic.model.Speciality;
 import com.springframework.petclinic.model.Vet;
+import com.springframework.petclinic.service.SpecialityService;
 import com.springframework.petclinic.service.VetService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
+    private final SpecialityService specialityService;
+
+    public VetMapService(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        if(vet.getSpecialities().size() > 0){
+            vet.getSpecialities().forEach(speciality -> {
+                //we need to save new speciality
+                if(speciality.getId() == null){
+                    Speciality savedSpeciality = specialityService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+                }
+            });
+        }
+        return super.save(vet);
+    }
 }
